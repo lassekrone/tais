28ae359d575c4ffa5a4153a29f8053191f177a0f
Jakob Tolstrup <jakob.l.tolstrup@gmail.com>
2024-11-14 20:58:56 +0100

    added yml file. updated the way we show pages

--- knowledge_gpt/main.py
+++ knowledge_gpt/main.py
@@ -18,31 +18,31 @@ from knowledge_gpt.core.embedding import embed_files
 from knowledge_gpt.core.qa import query_folder
 from knowledge_gpt.core.utils import get_llm
 
+from dotenv import load_dotenv
+import os
+
 
 EMBEDDING = "openai"
 VECTOR_STORE = "faiss"
-MODEL_LIST = ["gpt-3.5-turbo", "gpt-4"]
 
 # Uncomment to enable debug mode
 # MODEL_LIST.insert(0, "debug")
 
-st.set_page_config(page_title="KnowledgeGPT", page_icon="📖", layout="wide")
-st.header("📖KnowledgeGPT")
+load_dotenv()
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+
+st.set_page_config(page_title="T(AI)S", page_icon="📖", layout="wide")
+st.header("T(AIII)S")
+
 
 # Enable caching for expensive functions
 bootstrap_caching()
 
-sidebar()
-
-openai_api_key = st.session_state.get("OPENAI_API_KEY")
-
 
 if not openai_api_key:
-    st.warning(
-        "Enter your OpenAI API key in the sidebar. You can get a key at"
-        " https://platform.openai.com/account/api-keys."
-    )
-
+    st.error("OpenAI API key not found in .env file")
 
 uploaded_file = st.file_uploader(
     "Upload a pdf, docx, or txt file",
@@ -50,12 +50,11 @@ uploaded_file = st.file_uploader(
     help="Scanned documents are not supported yet!",
 )
 
-model: str = st.selectbox("Model", options=MODEL_LIST)  # type: ignore
+model = 'gpt-4' 
 
 with st.expander("Advanced Options"):
     return_all_chunks = st.checkbox("Show all chunks retrieved from vector search")
     show_full_doc = st.checkbox("Show parsed contents of the document")
-
 
 if not uploaded_file:
     st.stop()
@@ -109,13 +108,21 @@ if submit:
         llm=llm,
     )
 
+    source_pages = ''
+
+    
+    for source in result.sources:
+        formmated_source = source.metadata["source"].split('-')[0]
+        source_pages += formmated_source
+        
     with answer_col:
         st.markdown("#### Answer")
-        st.markdown(result.answer)
+        st.markdown(result.answer + ' - page(s): ' + source_pages)
 
     with sources_col:
         st.markdown("#### Sources")
         for source in result.sources:
             st.markdown(source.page_content)
-            st.markdown(source.metadata["source"])
+            formmated_source = source.metadata["source"].split('-')[0]
+            st.markdown('p. ' + formmated_source)
             st.markdown("---")
